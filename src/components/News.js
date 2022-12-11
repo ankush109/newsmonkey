6cf8e78c60d6e6b41cba33729a1cd9eb5aab0587
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2022-12-11 13:00:08 +0530

    dd

--- src/components/News.js
+++ src/components/News.js
@@ -1,89 +1,97 @@
-import React, { useEffect ,useState} from 'react'
-import NewsItem from './NewsItem'
-import Spinner from './Spinner';
-import PropTypes from 'prop-types'
-import InfiniteScroll from 'react-infinite-scroll-component';
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
+const News = (props) => {
+  const [articles, setarticles] = useState([]);
+  const [loading, setloading] = useState([true]);
+  const [page, setpage] = useState(1);
+  const [totalResults, settotalResults] = useState(0);
 
-const News =(props)=> {
-  const [articles,setarticles]=useState([])
-  const [loading,setloading]=useState([true])
-  const [page,setpage]=useState(1)
-  const [totalResults,settotalResults]=useState(0)
+  // document.title =`${props.category}-AnkNews `;
 
-        // document.title =`${props.category}-AnkNews `; 
-    
-  const  Updatenews =async()=>{
-      {
-        props.setprogress(0);
-        let url =`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=80d498a1f9ba4b9f90612aafd83680bc&page=${page}&pageSize=${props.pageSize}`;
-        setloading(true)
-        let data = await fetch(url);
-        let parseddata =await data.json()
-        props.setprogress(30)
-       setarticles(parseddata.articles)
-       settotalResults(parseddata.totalResults)
-       setloading(false)
-       props.setprogress(100);
+  const Updatenews = async () => {
+    {
+      props.setprogress(0);
+      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=80d498a1f9ba4b9f90612aafd83680bc&page=${page}&pageSize=${props.pageSize}`;
+      setloading(true);
+      let data = await fetch(url);
+      let parseddata = await data.json();
+      props.setprogress(30);
+      setarticles(parseddata.articles);
+      settotalResults(parseddata.totalResults);
+      setloading(false);
+      props.setprogress(100);
     }
-  }
-    useEffect(() => {
+  };
+  useEffect(() => {
     Updatenews();
-    }, [])
-  
- const fetchMoreData = async() => {
-     setpage(page +1)
-      {
-        let url =`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=80d498a1f9ba4b9f90612aafd83680bc&page=${page}&pageSize=${props.pageSize}`;
-        setloading(true)
-        let data = await fetch(url);
-        let parseddata =await data.json()
-        setarticles(articles.concat(parseddata.articles))
-        settotalResults(parseddata.totalResults)
-    };
-  }
-    
-    
+  }, []);
 
+  const fetchMoreData = async () => {
+    setpage(page + 1);
+    {
+      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=80d498a1f9ba4b9f90612aafd83680bc&page=${page}&pageSize=${props.pageSize}`;
+      setloading(true);
+      let data = await fetch(url);
+      let parseddata = await data.json();
+      setarticles(articles.concat(parseddata.articles));
+      settotalResults(parseddata.totalResults);
+    }
+  };
 
-      console.log("render");
-        return (
-            <div className="container my-3">
-              <h1 className="text-center"  style={{ margin: '35px 0px', marginTop:'90px ' }}>AnkNews - Top Headlines from {props.category} </h1>
-               {/* {state.loading && <Spinner/>} */}
-               <InfiniteScroll
-          dataLength={articles.length}
-          next={fetchMoreData}
-          hasMore={articles.length !==totalResults}
-          loader={<Spinner/>}
-        >
-          <div className="container">
-                <div className="row"> 
-                { articles.map((element)=>{
-                     return <div className="col-md-4" key={element.url} > 
-                      <NewsItem  description = {element.description?element.description.slice(0.50):""} title={element.title?element.title.slice(0,40):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
-                  </div>
-    })}
+  console.log("render");
+  return (
+    <div className="container my-3">
+      <h1
+        className="text-center"
+        style={{ margin: "35px 0px", marginTop: "90px " }}
+      >
+        AnkNews - Top Headlines from {props.category}{" "}
+      </h1>
+      {/* {state.loading && <Spinner/>} */}
+      <InfiniteScroll
+        dataLength={articles.length ? articles.length : 0}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    description={
+                      element.description ? element.description.slice(0.5) : ""
+                    }
+                    title={element.title ? element.title.slice(0, 40) : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                  />
                 </div>
-                </div>
-                </InfiniteScroll>
-                </div> 
-         
-        )
-    
-}
+              );
+            })}
+          </div>
+        </div>
+      </InfiniteScroll>
+    </div>
+  );
+};
 
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+News.propstypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 
-News.defaultProps ={
-  country:'in',
-  pageSize:8,
-  category:'general'
-
-}
-News.propstypes ={
- country :PropTypes.string,
- pageSize: PropTypes.number,
- category:PropTypes.string
-}
-
-export default News
+export default News;
